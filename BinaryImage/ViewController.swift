7375e8473b6be885e0fad6156995aef98f835626
taiwei.tseng <taiwei.tseng@rakuten.com>
2019-04-02 16:41:00 +0900

    Initial Commit

---
+++ BinaryImage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BinaryImage
+//
+//  Created by Tseng, TaiWei | Davis | TWR on 2019/04/02.
+//  Copyright © 2019 Davis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
